a73f3954ea61e31dde855ba83e7d1532cf09bbec
Kevin Ponce Moreira <kevinponcesite@gmail.com>
2021-12-22 12:56:41 -0500

    update

---
+++ E-commerc/src/App.jsx
@@ -0,0 +1,13 @@
+
+import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import Product from "./pages/Product";
+// import Register from "./pages/Register";
+// import ProductList from "./pages/ProductList";
+// import Cart from "./pages/Cart";
+
+const App = () => {
+  return <Home />;
+};
+
+export default App;
---
+++ E-commerc/src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://photy.org/tn_img/purple-background-lines-photy.org.jpg",
+        title: "SUMMER SALE",
+        desc: "CLOTING FOR SUMMER",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://www.wallpapertip.com/wmimgs/3-37860_hd-wallpapers-png.jpg",
+        title: "WINTER SALE",
+        desc: "CLOTING FOR WINTER",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://p4.wallpaperbetter.com/wallpaper/131/47/747/11-png-action-adventure-android-wallpaper-preview.jpg",
+        title: "POPULAR SALE",
+        desc: "CLOTING POPULAR",
+        bg: "fbf0f4",
+    }
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://m.media-amazon.com/images/I/61qMt8YrVtL._AC_UY550_.jpg",
+        title: "T-SHIRT WHITE"
+    },
+    {
+        id: 2,
+        img: "https://www.induvest.com.ec/wp-content/uploads/2021/04/CAMISA-JEAN-REFLECTIVA-FRENTE.jpg",
+        title: "T-SHIRT BLUE"
+    },
+    {
+        id: 3,
+        img: "https://image.dhgate.com/0x0s/f2-albu-g10-M00-9B-51-rBVaVlyktaOADdV8AADPRRhvDzY888.jpg/nuevos-hombres-camisa-para-hombre-de-manga.jpg",
+        title: "T-SHIRT RED AND BLACK"
+    }
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://www.zegna.com/bin/productImage.UVX44.SRF5.457.F.R6.jpeg"
+    },
+    {
+        id: 2,
+        img: "https://m.media-amazon.com/images/I/61SPXfIh-gL._AC_UY1000_.jpg"
+    },
+    {
+        id: 3,
+        img: "https://cdn.fashiola.mx/L556028607/newchic-hombre-manga-corta-camisas-de-diseno-casual-de-manga-corta-a-rayas-verticales-para-hombre.jpg"
+    },
+    {
+        id: 4,
+        img: "https://media.revistagq.com/photos/60950644235a5910299c9327/master/w_1600%2Cc_limit/hbeu50451093_100_100.jpg"
+    },
+    {
+        id: 5,
+        img: "https://uniforma.net/86-superlarge_default/camisa-trabajo-manga-larga-de-algodon-uniforma.jpg"
+    },
+    {
+        id: 6,
+        img: "https://suministrales.com/wp-content/uploads/2019/11/Camisas-Fil-a-Fil-y-Twill.png"
+    },
+    {
+        id: 7,
+        img: "https://uniforma.net/86-superlarge_default/camisa-trabajo-manga-larga-de-algodon-uniforma.jpg"
+    },
+    {
+        id: 8,
+        img: "https://cdn3.vestuariolaboral.com/31161-large_default/camisa-de-trabajo-manga-larga-monza-02016.jpg"
+    }
+]
